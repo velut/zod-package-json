9c2a0e98bb663877edcec35a31a6677e5e0bb0dc
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-08-01 21:00:59 +0200

    docs: update @packageDocumentation

--- src/mini/index.ts
+++ src/mini/index.ts
@@ -1,17 +1,17 @@
 /**
-This package exports a zod mini schema (and a TypeScript type) named `PackageJson`
+This package exports a Zod mini schema (and the inferred TypeScript type) named `PackageJson`
 that can parse most well-formed `package.json` files.
 
 The schema includes all currently supported properties listed in the
-{@link https://docs.npmjs.com/cli/v10/configuring-npm/package-json/ | npm docs}
+{@link https://docs.npmjs.com/cli/v11/configuring-npm/package-json | npm docs}
 as well as additional well-known properties specific to TypeScript and Node.js.
 
 The schema also preserves unknown properties, which can be accessed
 by indexing the parsed data with the desired string keys.
 
-The schema only validates the listed properties against the expected type
+The schema only validates the known properties against their expected type
 but does not do any additional normalization such as merging similar properties.
-If necessary, that can be done by extending the schema with zod mini methods.
+If necessary, that can be done by extending the schema with Zod mini functions.
 
 @example
 
